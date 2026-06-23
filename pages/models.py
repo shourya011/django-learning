f7ebab68efe67d284b3239e33f544956c6b32bfa
Shourya Sharma <shouryano01@gmail.com>
2026-06-23 14:44:55 +0530

    Model → view → template, the full data pipeline

--- pages/models.py
+++ pages/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pages.models import Movie
 
 # Create your views here.
+# def home(request):
+#     return render(request,'home.html',{'username': 'vinu'})
+
 def home(request):
-    return render(request,'home.html',{'username': 'vinu'})
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies,
+    }
+    return render(request,'home.html',context)
 
 def greet(request,name):
     return HttpResponse(f"Hello {name}")

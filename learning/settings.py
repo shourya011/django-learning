2720ef92e8b699bd1d306e840685beee473a16a8
Shourya Sharma <shouryano01@gmail.com>
2026-06-25 15:14:32 +0530

    CSS and static setting

--- learning/settings.py
+++ learning/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'learning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR ,'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'learning' / 'static'
+]
